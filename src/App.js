27cda8f88143bb62b984ccdf4f0c75102f99256d
mmccarthy64 <mmccarthy64@hotmail.com>
2021-11-19 08:55:09 -0600

    Create Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     //BEM
     <div className="app">
-      hi
+      {/* Home */}
+      <Home />
+        {/* Header */}
+
+        {/* Banner */}
+          {/* Search */}
+
+        {/* Cards */}
+
+        {/* Footer */}
+      {/* Search Page */}
+        {/* ... */}
     </div>
   );
 }
