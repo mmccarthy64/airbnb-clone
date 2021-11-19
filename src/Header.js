da0d77552233761668c8b3a1b82406a5da4848a4
mmccarthy64 <mmccarthy64@hotmail.com>
2021-11-19 09:12:07 -0600

    Create Header comp

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header_icon'
+                src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dezeen.com%2F2014%2F07%2F16%2Fairbnb-rebrand-designstudio-logo-belo%2F&psig=AOvVaw0YfgoRybZLiujgK0EQNEAA&ust=1637420250696000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiBqPXXpPQCFQAAAAAdAAAAABAD'/>
+        </div>
+    )
+}
+
+export default Header
